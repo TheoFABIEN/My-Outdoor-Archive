76869caad136ab25cbd41ac9dd29e6ff504ffd3c
Théo Fabien <theo.fabien@inrae.fr>
2026-03-16 10:45:02 +0100

    Fixed init.sql

--- init/ini.sql
+++ init/ini.sql
@@ -1,11 +1,29 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
 
-CREATE TABLE hikes (
+CREATE TABLE IF NOT EXISTS points (
     id SERIAL PRIMARY KEY,
     name TEXT,
     difficulty INTEGER,
-    affluence INTEGER,
     gaz BOOLEAN,
     notes TEXT,
     geom GEOMETRY(Point, 4326)
+);
+
+CREATE TABLE IF NOT EXISTS areas (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    notes TEXT,
+    geom GEOMETRY(Polygon, 4326)
+);
+
+CREATE TABLE IF NOT EXISTS gpx_hikes (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    difficulty INTEGER,
+    gaz BOOLEAN,
+    notes TEXT,
+    distance_km FLOAT,
+    elevation_gain INTEGER,
+    elevation_loss INTEGER,
+    geom GEOMETRY(LineString, 4326)
 );
